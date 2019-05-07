13c97ecd54bc64bc12a83a266bc2869da96aafbf
johnhmacleod <john.h.macleod@gmail.com>
2019-05-07 15:38:50 +1000

    Update preprocessor.py

--- iotfunctions/preprocessor.py
+++ iotfunctions/preprocessor.py
@@ -567,8 +567,8 @@ class PredictPower(BaseTransformer):
             print(rs)
             result = json.loads(rs)
             print(result)
-            print(result.get('fields')
-            print(result.get('vvalues')
+            print(result.get('fields'))
+            print(result.get('vvalues'))
             print(result.get('vvalues')[0])
         for index, row in df.head().iterrows():
             print(row)
