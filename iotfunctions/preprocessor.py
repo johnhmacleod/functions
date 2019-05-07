6d959e12d4397f4c3a9a40bde90ae9c907748eb6
johnhmacleod <john.h.macleod@gmail.com>
2019-05-07 14:44:13 +1000

    Update preprocessor.py

--- iotfunctions/preprocessor.py
+++ iotfunctions/preprocessor.py
@@ -554,17 +554,20 @@ class PredictPower(BaseTransformer):
         
         for index, row in df.head().iterrows():
             # print(row)
-            print(index, row[self.temperature], row[self.humidity], row[self.hourofday])
+            print(row[self.temperature], row[self.humidity], row[self.hourofday])
         
-        #payload_scoring = {"fields": ["AVGTEMP", "AVGHUMIDITY", "HOUROFDAY"], "values": [[df[self.temperature],df[self.humidity],df[self.hourofday]]]}
+            payload_scoring = {"fields": ["AVGTEMP", "AVGHUMIDITY", "HOUROFDAY"], "values": [[row[self.temperature],row[self.humidity],row[self.hourofday]]]}
 
-        #response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/v3/wml_instances/c406a8c1-5aae-4934-887a-29871d186f00/deployments/c69641c7-65d1-43d6-a539-0d92147f49a9/online', json=payload_scoring, headers=header)
-        # print("Scoring response")
-        # print(json.loads(response_scoring.text))    
+            response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/v3/wml_instances/c406a8c1-5aae-4934-887a-29871d186f00/deployments/c69641c7-65d1-43d6-a539-0d92147f49a9/online', json=payload_scoring, headers=header)
+            print("Scoring response")
+            print(json.loads(response_scoring.text))    
         
-        #df[self.output_item] = response_scoring.values[0][4];
-        
-        df[self.predictedpower] = df[self.temperature] * df[self.humidity] / 100
+            row[self.output_item] = response_scoring.values[0][4];
+            
+        for index, row in df.head().iterrows():
+            print(row)
+
+        # df[self.predictedpower] = df[self.temperature] * df[self.humidity] / 100
         return df 
     
     
